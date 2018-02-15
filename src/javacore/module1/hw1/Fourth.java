c031296af77763d5a11d354e16f062496fffc226
Dobryvechir <dobrive4ir@gmail.com>
2018-02-15 21:01:20 +0200

    1st homework edited

--- src/javacore/module1/hw1/Fourth.java
+++ src/javacore/module1/hw1/Fourth.java
@@ -1,8 +1,5 @@
 package javacore.module1.hw1;
 
-/**
- * Created by Dobryve4ir on 28.01.2017.
- */
 public class Fourth {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
