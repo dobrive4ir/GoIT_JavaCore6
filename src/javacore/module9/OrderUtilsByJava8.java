1a04bee7429189c6b9ca8d9586e25ede1d62e095
Dobryvechir <dobrive4ir@gmail.com>
2018-02-21 21:26:24 +0200

    module 9: TODO edited

--- src/javacore/module9/OrderUtilsByJava8.java
+++ src/javacore/module9/OrderUtilsByJava8.java
@@ -9,6 +9,10 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ *
+ */
+
 public final class OrderUtilsByJava8 {
 
     private OrderUtilsByJava8() {
